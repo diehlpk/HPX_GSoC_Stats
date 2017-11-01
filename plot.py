21c9cc454193cd7b779a0ebce1c3c6fd4243269b
Patrick Diehl <diehl@ins.uni-bonn.de>
2017-10-31 21:52:06 -0400

    Update plots

--- plot.py
+++ plot.py
@@ -13,9 +13,11 @@ with open('data.dat', 'rb') as csvfile:
 	for row in spamreader:
 		data.append(np.array(row,dtype=int))
 		
-		
+plt.xkcd()
+
 mpl_fig = plt.figure()
 ax = mpl_fig.add_subplot(111)
+plt.gcf().subplots_adjust(bottom=0.15)
 
 year = column(data,0)
 all = column(data,1)
@@ -35,4 +37,26 @@ plt.xticks(ind , year)
 plt.yticks(np.arange(0, max(all)+2, 1))
 plt.legend((p1[0], p2[0]), ('Pass', 'Fail'),loc=2)
 
-plt.savefig('students_per_year.png')
+plt.savefig('students_per_year.png', format='png', dpi=150)
+
+plt.close()
+
+
+request = column(data,4)
+merged = column(data,5)
+
+ind = np.arange(len(year)) 
+width = 0.35      
+
+p1 = plt.bar(ind, request, width, color='#d62728')
+p2 = plt.bar(ind, merged, width)
+plt.gcf().subplots_adjust(bottom=0.15)
+
+plt.ylabel('Pull requests')
+plt.xlabel('Year')
+plt.title('Pull requests and merged pull requests')
+plt.xticks(ind , year)
+plt.yticks(np.arange(0, max(request)+2, 5))
+plt.legend((p1[0], p2[0]), ('Requested', 'Merged'),loc=2)
+
+plt.savefig('pull_per_year.png', format='png', dpi=150)
